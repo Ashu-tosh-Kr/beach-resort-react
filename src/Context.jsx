fa5efa7a633544c88f76c31e301b55b2f168087d
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2020-07-24 10:27:03 +0530

    refactoring code

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 //import items from './data.js';
-import Client from './Contentful';
-
+import Client from "./Contentful";
 
 const RoomContext = React.createContext();
 
 function RoomProvider({ children }) {
-
   const [data, setData] = useState({
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
-    type: 'all',
+    type: "all",
     capacity: 1,
     price: 0,
     minPrice: 0,
@@ -26,14 +24,14 @@ function RoomProvider({ children }) {
   async function getData() {
     try {
       let res = await Client.getEntries({
-        content_type: 'beachResortRoom',
-        order: 'fields.price',
+        content_type: "beachResortRoom",
+        order: "fields.price",
       });
 
       let rooms = formatData(res.items);
-      let featuredRooms = rooms.filter(room => room.featured);
-      let maxPrice = Math.max(...rooms.map(item => item.price));
-      let maxSize = Math.max(...rooms.map(item => item.size));
+      let featuredRooms = rooms.filter((room) => room.featured);
+      let maxPrice = Math.max(...rooms.map((item) => item.price));
+      let maxSize = Math.max(...rooms.map((item) => item.size));
 
       setData({
         ...data,
@@ -56,8 +54,8 @@ function RoomProvider({ children }) {
     // let maxPrice = Math.max(...rooms.map(item => item.price));
     // let maxSize = Math.max(...rooms.map(item => item.size));
 
-    // setData({
-    //   ...data,
+    // setData(prev=>({
+    //   ...prev,
     //   rooms: rooms,
     //   featuredRooms: featuredRooms,
     //   sortedRooms: rooms,
@@ -65,45 +63,50 @@ function RoomProvider({ children }) {
     //   price: maxPrice,
     //   maxPrice: maxPrice,
     //   maxSize: maxSize,
-    // });
+    // }));
     getData();
-  }, [])
+  }, []);
 
   function formatData(items) {
-    let tempItems = items.map(item => {
+    let tempItems = items.map((item) => {
       let id = item.sys.id;
-      let images = item.fields.images.map(img => img.fields.file.url);
+      let images = item.fields.images.map((img) => img.fields.file.url);
       let room = { ...item.fields, images: images, id };
       return room;
-    })
+    });
     return tempItems;
   }
 
   function getRoom(slug) {
     const tempRooms = [...data.rooms];
-    const room = tempRooms.find(room => room.slug === slug);
+    const room = tempRooms.find((room) => room.slug === slug);
     return room;
   }
 
   function handleChange(e) {
     const target = e.target;
     const name = target.name;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
 
-    setData({
-      ...data,
+    setData((prev) => ({
+      ...prev,
       [name]: value,
-    });
-
+    }));
   }
 
   useEffect(() => {
     filterRooms();
-  }, [data.type, data.capacity, data.price, data.minSize, data.maxSize, data.breakfast, data.pets]);
+  }, [
+    data.type,
+    data.capacity,
+    data.price,
+    data.minSize,
+    data.maxSize,
+    data.breakfast,
+    data.pets,
+  ]);
 
   function filterRooms() {
-    let flag = false;
-
     let {
       rooms,
       type,
@@ -123,55 +126,48 @@ function RoomProvider({ children }) {
     price = +price;
 
     //Filter by type
-    if (type !== 'all') {
+    if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
-      flag = true;
     }
 
     //Filter by capacity
     if (capacity !== 1) {
       tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
-      flag = true;
     }
 
     //Filter by price
     if (price !== 0) {
       tempRooms = tempRooms.filter((room) => room.price <= price);
-      flag = true;
     }
 
     //Filter by size
     if (minSize !== 0 && maxSize !== 0) {
-      tempRooms = tempRooms.filter((room) => room.size >= minSize && room.size <= maxSize);
-      flag = true;
+      tempRooms = tempRooms.filter(
+        (room) => room.size >= minSize && room.size <= maxSize
+      );
     }
 
     //Filter by breakfast
     if (breakfast) {
       tempRooms = tempRooms.filter((room) => room.breakfast === true);
-      flag = true;
     }
 
     //Filter by pets
     if (pets) {
       tempRooms = tempRooms.filter((room) => room.pets === true);
-      flag = true;
     }
 
-    if (flag) {
-      setData({
-        ...data,
-        sortedRooms: tempRooms,
-      })
-    }
+    setData((prev) => ({
+      ...prev,
+      sortedRooms: tempRooms,
+    }));
   }
-
 
   return (
     <RoomContext.Provider value={{ ...data, getRoom: getRoom, handleChange }}>
       {children}
     </RoomContext.Provider>
-  )
+  );
 }
 
 export { RoomContext, RoomProvider };
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import defaultImage from '../images/room-1.jpeg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import defaultImage from "../images/room-1.jpeg";
+import { Link } from "react-router-dom";
 
 export default function Room({ room }) {
   const { images, name, slug, price } = room;
   return (
-    <article className='room'>
+    <article className="room">
       <div className="img-container">
         <img src={images[0] || defaultImage} alt="single room" />
 
@@ -13,9 +13,11 @@ export default function Room({ room }) {
           <h6>${price}</h6>
           <p>per night</p>
         </div>
-        <Link to={`/rooms/${slug}`} className='btn-primary room-link'>Features</Link>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
       </div>
-      <p className='room-info'>{name}</p>
+      <p className="room-info">{name}</p>
     </article>
-  )
+  );
 }
